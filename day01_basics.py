3fec272a09ac06f070e2e5d8ac464d0167b1b9ad
arunam0310-collab <arunam0310@gmail.com>
2026-08-29 11:11:12 +0530

    Update day01_basics.py

--- day01_basics.py
+++ day01_basics.py
@@ -6,3 +6,11 @@ name=input("What is your name? ")
 age=int(input (" How old are you? "))
 fav_col=input (" What is your favourite colour? ")
 print(f"My name is {name}. I am {age} years old and my favourite colour is {fav_col}.")
+
+# challenge 3
+num1 = int(input("Enter number 1: "))
+num2 = int(input("Enter number 2: "))
+
+print("Sum:", num1 + num2)
+print("Difference:", num1 - num2)
+print("Product:", num1 * num2)
