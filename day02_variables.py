2d193efef687df38d080f2db07fb055118c42675
arunam0310-collab <arunam0310@gmail.com>
2026-08-29 11:33:41 +0530

    Update day02_variables.py

--- day02_variables.py
+++ day02_variables.py
@@ -4,3 +4,12 @@ name = "Lily"
 age = 17
 print(name)
 print(age)
+
+# Challenge 2
+
+price = int(input("Enter the price:"))
+quantity = int(input("Enter the quantity:"))
+
+print("Total cost:", price * quantity)
+
+# challenge 3
