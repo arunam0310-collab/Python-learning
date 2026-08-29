11e472045d9b5c3db9a82067ecc10587dbb9a737
arunam0310-collab <arunam0310@gmail.com>
2026-08-29 11:06:06 +0530

    Update day01_basics.py

--- day01_basics.py
+++ day01_basics.py
@@ -1,2 +1,8 @@
 # challenge 1
 print("Hello! This is my first Python project.")
+
+# challenge 2
+name=input("What is your name? ")
+age=int(input (" How old are you? "))
+fav_col=input (" What is your favourite colour? ")
+print(f"My name is {name}. I am {age} years old and my favourite colour is {fav_col}.")
