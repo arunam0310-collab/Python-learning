ca10a266eeca37dfc5e8926c4c395b81620ff59b
arunam0310-collab <arunam0310@gmail.com>
2026-08-29 11:38:22 +0530

    Update day02_variables.py

--- day02_variables.py
+++ day02_variables.py
@@ -13,3 +13,10 @@ quantity = int(input("Enter the quantity:"))
 print("Total cost:", price * quantity)
 
 # challenge 3
+
+num = int(input("Enter a number:"))
+
+if num % 2 == 0:
+    print("The number is even")
+else:
+    print("The number is odd")
