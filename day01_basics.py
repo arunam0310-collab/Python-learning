3bbd91739cf9d5183c54b7440a9c6153212715a4
arunam0310-collab <arunam0310@gmail.com>
2026-08-29 11:19:15 +0530

    Update day01_basics.py

--- day01_basics.py
+++ day01_basics.py
@@ -1,16 +1,34 @@
 # challenge 1
+
 print("Hello! This is my first Python project.")
 
 # challenge 2
+
 name=input("What is your name? ")
 age=int(input (" How old are you? "))
 fav_col=input (" What is your favourite colour? ")
 print(f"My name is {name}. I am {age} years old and my favourite colour is {fav_col}.")
 
 # challenge 3
+
 num1 = int(input("Enter number 1: "))
 num2 = int(input("Enter number 2: "))
 
 print("Sum:", num1 + num2)
 print("Difference:", num1 - num2)
 print("Product:", num1 * num2)
+ 
+# Challenge 4
+
+name="Lily"
+age=17
+city="New York"
+fav_food="Ramyeon"
+fav_sub="Maths"
+print("=====MY PROFILE====")
+print("Name:", name)
+print ("Age:", age)
+print ("City:", city)
+print("Favorite Food:", fav_food)
+print ("Favorite Subject:", fav_sub)
+print ("===================")
