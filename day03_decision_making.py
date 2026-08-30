b979ea1e368c3d002a4e038ee42038353d0d21f1
arunam0310-collab <arunam0310@gmail.com>
2026-08-30 16:39:40 +0530

    Update day03_decision_making.py

--- day03_decision_making.py
+++ day03_decision_making.py
@@ -43,3 +43,14 @@ else:
 print("Fail")
 
 # Challenge 4
+
+num1=int(input("Enter the first number:"))
+num2=int(input ("Enter the second number:"))
+if num1>num2:
+print("First number is greater")
+elif num2>num1:
+print ("Second number is greater")
+else:
+print ("Two numbers are equal")
+
+# Challenge 5
