49b19a51da27a6e1a8bc1c2cbe7aa8c04472fe03
arunam0310-collab <arunam0310@gmail.com>
2026-08-30 17:07:35 +0530

    Update day02_variables.py

--- day02_variables.py
+++ day02_variables.py
@@ -2,15 +2,15 @@
 
 name = "Lily"
 age = 17
-print(name)
-print(age)
+   print(name)
+   print(age)
 
 # Challenge 2
 
 price = int(input("Enter the price:"))
 quantity = int(input("Enter the quantity:"))
 
-print("Total cost:", price * quantity)
+   print("Total cost:", price * quantity)
 
 # challenge 3
 
@@ -24,24 +24,24 @@ if num % 2 == 0:
 num1 = float(input("Enter number 1:"))
 num2 = float(input("Enter number 2:"))
 
-print("Addition:", num1 + num2)
-print("Subtraction:", num1 - num2)
-print("Multiplication:", num1 * num2)
-print("Division:", num1 / num2)
-print("Reminder:", num1 % num2)
+   print("Addition:", num1 + num2)
+   print("Subtraction:", num1 - num2)
+   print("Multiplication:", num1 * num2)
+   print("Division:", num1 / num2)
+   print("Reminder:", num1 % num2)
 
 # Challenge 5
 
 cel = int(input("Enter the celsius:"))
 far = (cel * 9 / 5) + 32
 
-print("Fahrenheit:", far)
+   print("Fahrenheit:", far)
 
 # Challenge 6
 
 sec = int(input("Enter the seconds:"))
 minutes = sec // 60
 re_sec = sec % 60
-
-print("Minutes:", minutes)
-print("Remaining seconds:", re_sec)
+ 
+   print("Minutes:", minutes)
+   print("Remaining seconds:", re_sec)
