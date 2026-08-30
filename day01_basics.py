bf9b8f9d2291fdb1224138ca79f934b14f894ac4
arunam0310-collab <arunam0310@gmail.com>
2026-08-30 17:10:30 +0530

    Update day01_basics.py

--- day01_basics.py
+++ day01_basics.py
@@ -1,21 +1,21 @@
 # challenge 1
 
-print("Hello! This is my first Python project.")
+   print("Hello! This is my first Python project.")
 
 # challenge 2
 
 name=input("What is your name? ")
 age=int(input (" How old are you? "))
 fav_col=input (" What is your favourite colour? ")
-print(f"My name is {name}. I am {age} years old and my favourite colour is {fav_col}.")
+    print(f"My name is {name}. I am {age} years old and my favourite colour is {fav_col}.")
 
 # challenge 3
 
 num1 = int(input("Enter number 1: "))
 num2 = int(input("Enter number 2: "))
-print("Sum:", num1 + num2)
-print("Difference:", num1 - num2)
-print("Product:", num1 * num2)
+    print("Sum:", num1 + num2)
+    print("Difference:", num1 - num2)
+    print("Product:", num1 * num2)
  
 # Challenge 4
 
@@ -24,25 +24,25 @@ age=17
 city="New York"
 fav_food="Ramyeon"
 fav_sub="Maths"
-print("=====MY PROFILE====")
-print("Name:", name)
-print ("Age:", age)
-print ("City:", city)
-print("Favorite Food:", fav_food)
-print ("Favorite Subject:", fav_sub)
-print ("===================")
+    print("=====MY PROFILE====")
+    print("Name:", name)
+    print ("Age:", age)
+    print ("City:", city)
+    print("Favorite Food:", fav_food)
+    print ("Favorite Subject:", fav_sub)
+    print ("===================")
 
 # Challenge 5
 
 len = int(input("Enter the length:"))
 wid = int(input("Enter the width:"))
-print("Area:", len * wid)
-print("Perimeter:", 2 * (len + wid))
+    print("Area:", len * wid)
+    print("Perimeter:", 2 * (len + wid))
 
 # Challenge 6
 
 name=input ("Enter the name:")
 fav_num=int(input ("Enter a number:"))
-print ("Hello", name, "!")
-print ("Your favorite number is", fav_num)
-print("If we add 10 to it, we get", 10+fav_num)
+    print ("Hello", name, "!")
+    print ("Your favorite number is", fav_num)
+    print("If we add 10 to it, we get",10+fav_num) 
