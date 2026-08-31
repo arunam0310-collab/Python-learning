86073089a25d7c9629d2364bbcb31969e6aca00e
arunam0310-collab <arunam0310@gmail.com>
2026-08-31 20:54:48 +0530

    Update day_4_revision.py

--- day_4_revision.py
+++ day_4_revision.py
@@ -9,3 +9,12 @@ else:
    print("Negative")
 
 # Challenge 2
+
+name=input("Enter the name:")
+mark=int(input ("Enter the mark:"))
+if mark>=35:
+   print(name,"-Pass")
+else:
+   print (name,"-Fail")
+
+# Challenge 3
